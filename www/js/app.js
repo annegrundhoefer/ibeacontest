e47248ff86eab8ba3d225553101dbe36d4120155
Jon Senterfitt <thetwosents@gmail.com>
2015-11-01 22:14:12 -0600

    Update

--- www/js/app.js
+++ www/js/app.js
@@ -31,16 +31,16 @@ angular.module('starter', ['ionic','ngCordovaBeacon'])
  
         $cordovaBeacon.requestWhenInUseAuthorization();
  
-//         $rootScope.$on("$cordovaBeacon:didRangeBeaconsInRegion", function(event, pluginResult) {
-//             var uniqueBeaconKey;
-//             for(var i = 0; i < pluginResult.beacons.length; i++) {
-//                 uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
-//                 $scope.beacons[uniqueBeaconKey] = pluginResult.beacons[i];
-//             }
-//             $scope.$apply();
-//         });
+        $rootScope.$on("$cordovaBeacon:didRangeBeaconsInRegion", function(event, pluginResult) {
+            var uniqueBeaconKey;
+            for(var i = 0; i < pluginResult.beacons.length; i++) {
+                uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
+                $scope.beacons[uniqueBeaconKey] = pluginResult.beacons[i];
+            }
+            $scope.$apply();
+        });
  
-//         $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("estimote", "b9407f30-f5f8-466e-aff9-25556b57fe6d"));
+        $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("estimote", "b9407f30-f5f8-466e-aff9-25556b57fe6d"));
  
     });
 });
